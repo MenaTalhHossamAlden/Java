998ea62e4b75434389717d6d5929c4b45ef7baeb
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-08-28 13:54:40 +0300

    Data Types

---
+++ 03 - Data Types.java
@@ -0,0 +1,11 @@
+class DataTypes {
+    public static void main(String[] args) {
+        double number1 = 5.8;
+        int number2 = (int)number1;
+
+        System.out.println(number2);
+    }
+}   
+
+// implicit casting (also known as widening conversion) occurs automatically when you convert a smaller or less precise data type to a larger or more precise one.
+// otherwise, you will need to make casting explicitly.
