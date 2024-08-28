5f34570ab27830497389112c8c272bfaeae8fe28
MenatalhHossamalden <146476199+MenaTalhHossamAlden@users.noreply.github.com>
2024-08-28 14:17:43 +0300

    Operators

---
+++ 04 - Operators.java
@@ -0,0 +1,23 @@
+class HelloWorld {
+    public static void main(String[] args) {
+        // assignment
+        int number1 = 12;
+        int number2 = 6;
+
+        // arithmetic 
+        // addition
+        System.out.println(number1 + number2); // 18
+
+        // subtraction
+        System.out.println(number1 - number2); // 6
+
+        // multiplication
+        System.out.println(number1 * number2); // 72
+
+        // division
+        System.out.println(number1 / number2); // 2
+
+        // remainder (modulo/modulus)
+        System.out.println(number1 % number2); // 0
+    }
+}   
